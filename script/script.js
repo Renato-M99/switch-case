92b8a2d8416b552502aea8e115bd2d8052c47cf2
Renato <renatojunior7@hotmail.com>
2024-03-19 20:38:02 -0300

    switch-case

---
+++ script/script.js
@@ -0,0 +1,47 @@
+const frutas = document.querySelectorAll(".fruta");
+
+frutas.forEach((button) => {
+    /* addEventListener = 'ouve' os eventos do usuário e diparada a ação quando o evento especificado for disparado,
+    nesse caso será o evento "click". */
+    button.addEventListener("click", () => {
+
+        document.querySelector("header").style.color = "white";
+        document.querySelector("footer").style.color = "white";
+
+        switch (button.innerText) {
+            case "🍌":
+                document.body.style.backgroundColor = "#f1c329";
+                document.querySelector("header").style.color = "black";
+                document.querySelector("footer").style.color = "black";
+                break;
+
+            case "🍉":
+                document.body.style.backgroundColor = "#f22d2d";
+                break;
+            case "🍇":
+                document.body.style.backgroundColor = "#835AB8";
+                break;
+
+            case "🍊":
+                document.body.style.backgroundColor = "#F8762A";
+                break;
+            case "🥑":
+                document.body.style.backgroundColor = "#059a23";
+                break;
+
+            case "🍈":
+                document.body.style.backgroundColor = "#a9d399";
+                document.querySelector("header").style.color = "black";
+                document.querySelector("footer").style.color = "black";
+                break;
+
+            default:
+                console.log("Botão não reconhecido: ", button.innerText);
+
+        }
+
+    })
+
+
+
+})
